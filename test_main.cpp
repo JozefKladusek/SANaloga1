ee52b4f045cb12db5691cc0e34e66495cb0fa5cc
Jozef <jozef.kladusek@student.um.si>
2024-04-14 17:13:07 +0200

    senovo

--- test_main.cpp
+++ test_main.cpp
@@ -18,44 +18,6 @@ void runTests() {
         std::cout << "0 + 0 != 0 (FAIL)" << std::endl;
     }
 
-    // Test odstej function
-    std::cout << "Testing odstej function:" << std::endl;
-    result = odstej(1, 2);
-    if (result == -1) {
-        std::cout << "1 - 2 = -1 (PASS)" << std::endl;
-    } else {
-        std::cout << "1 - 2 != -1 (FAIL)" << std::endl;
-    }
-    
-    result = odstej(0, 0);
-    if (result == 0) {
-        std::cout << "0 - 0 = 0 (PASS)" << std::endl;
-    } else {
-        std::cout << "0 - 0 != 0 (FAIL)" << std::endl;
-    }
-
-    // Test zmnozi function
-    std::cout << "Testing zmnozi function:" << std::endl;
-    result = zmnozi(1, 2);
-    if (result == 2) {
-        std::cout << "1 * 2 = 2 (PASS)" << std::endl;
-    } else {
-        std::cout << "1 * 2 != 2 (FAIL)" << std::endl;
-    }
-    
-    result = zmnozi(0, 0);
-    if (result == 0) {
-        std::cout << "0 * 0 = 0 (PASS)" << std::endl;
-    } else {
-        std::cout << "0 * 0 != 0 (FAIL)" << std::endl;
-    }
-    
-    result = zmnozi(1, 0);
-    if (result == 0) {
-        std::cout << "1 * 0 = 0 (PASS)" << std::endl;
-    } else {
-        std::cout << "1 * 0 != 0 (FAIL)" << std::endl;
-    }
 }
 
     int main() {
